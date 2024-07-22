31b415523b73e09c006b96b0e2e4583bd1e8da34
JehuReyna <162845212+JehuReyna@users.noreply.github.com>
2024-07-22 07:45:59 -0600

    Avance

--- resources/views/pagClients/AcercaDeNosotros.blade.php
+++ resources/views/pagClients/AcercaDeNosotros.blade.php
@@ -1,3 +1,48 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
+<!DOCTYPE html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('/css/StylePages.css')}}">
+    <title>Document</title>
+</head>
+<body id="body">
+
+    
+{{-- HEADER --}}
+<div >
+    @include("components.clients-component.header")
 </div>
+
+{{-- DONDE NOS ENCONTRAMOS --}}
+<div class="card text-bg-dark" id="sombras">
+    <div class="card text-center text-bg-dark">
+        <div class="card-header">
+            <h3>¿DONDE NOS ENCONTRAMOS?</h3>
+        </div>
+        <div class="card-body">
+            <div class="row me-1">
+                <img src="{{url('/img/mapa.png')}}" height="300" alt=""> 
+            </div>    
+            <div class="mt-1">
+                2 de Abril 105, Los Pinos, 93557 Gutiérrez Zamora, Ver.
+            </div>                           
+        </div>
+    </div>
+</div>
+
+    {{-- FOOTER --}}
+    <div>
+        @include("components.clients-component.footer ")
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</body>
+</html>
--- resources/views/pagClients/Inicio.blade.php
+++ resources/views/pagClients/Inicio.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -305,6 +305,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </body>
 </html>
 
