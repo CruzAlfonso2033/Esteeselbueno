5276d9a35c8c78b3ba294b042098e6a040beab18
JehuReyna <162845212+JehuReyna@users.noreply.github.com>
2024-08-06 12:04:51 -0600

    Ahora sí

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/welcome', function () {
-    return view('welcome');
-}); */
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 
 
 
